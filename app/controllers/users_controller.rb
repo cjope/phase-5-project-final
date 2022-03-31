9595e62143fdca5e51b79d29a4a32c20d2c61d02
Craig <craigjope@gmail.com>
2022-03-31 12:19:32 -0600

    user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
-end
+    rescue_from ActiveRecord::RecordInvalid, with: :render_not_found
+    
+        def index
+          render json: User.all
+        end
+        
+        def show
+          user = User.find_by(id: session[:user_id])
+          render json: user
+        end
+        
+        def create
+          user = User.create!(user_params)
+          user.valid?
+          session[:user_id] = user.id
+          render json: user, status: :created
+        end
+        
+        def update
+          user = User.find_by(id: session[:user_id])
+          user.update!(user_params)
+          render json: user, status: :ok
+        end
+        
+        def destroy
+          user = User.find_by(id: params[:id])
+          user.destroy!
+          head :no_content
+        end
+
+        private
+        
+          def user_params
+            params.permit(:username, :first_name, :last_name, :password, :password_confirmation,  :age, :bio, :email)
+          end
+        
+          def user_update_params
+            params.permit(:username, :first_name, :last_name, :password, :password_confirmation,  :age, :bio, :email)
+          end
+    
+          def render_not_found(invalid)
+            render json: { errors: invalid}
+          end
+        
+    end
